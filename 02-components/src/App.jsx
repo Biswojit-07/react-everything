5dac27ce35da52010e81607d945b135b9b841ec5
Biswojit-07 <your-email-linked-to-github>
2025-10-28 20:25:23 +0530

    04-project done

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './components/Card'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <>
+    <Navbar /> 
+    <Card name='Bhola' />
+    <Card name='Rahul' />
+    </>
+  )
+}
+export default App
---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,8 @@
+function Card(props){
+  return(
+<div className='card'>
+      <h1>Hello, My name is {props.name}!</h1>
+      <p>damn true this is awesome!</p>
+    </div>  )
+}
+export default Card
